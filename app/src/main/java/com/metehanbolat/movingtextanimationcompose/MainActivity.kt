bd1c4eafd7ef89b3d460ab620d6cead2ecf0cc2b
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-07-10 15:46:42 +0300

    Application was completed.

--- app/src/main/java/com/metehanbolat/movingtextanimationcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/movingtextanimationcompose/MainActivity.kt
@@ -3,14 +3,16 @@ package com.metehanbolat.movingtextanimationcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import com.metehanbolat.movingtextanimationcompose.ui.theme.MovingTextAnimationComposeTheme
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovingTextAnimationComposeTheme {
-
+                MainScreen()
             }
         }
     }
